d6b1a8b6b8804036bf1a209287861c0fca51fab2
JACEK\Jacek <jaca1180@op.pl>
2017-06-03 06:19:29 +0200

    kolejne zmiany wyglądu

--- Spotkanie/app/src/main/java/jacek/spotkanie/MainActivity.java
+++ Spotkanie/app/src/main/java/jacek/spotkanie/MainActivity.java
@@ -1,5 +1,6 @@
 package jacek.spotkanie;
 
+        import android.content.Intent;
         import android.support.design.widget.FloatingActionButton;
         import android.support.design.widget.Snackbar;
         import android.support.v7.app.AppCompatActivity;
@@ -131,8 +132,9 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
-                    final String item = (String) parent.getItemAtPosition(position);
-                    //jakis kod do obsługi przyciśnięcia
+                    final EventInfo item = (EventInfo) parent.getItemAtPosition(position);
+                    //Intent i = new Intent(this, InfoActivity.class);
+                    //startActivity(i);
                 }
 
             });
